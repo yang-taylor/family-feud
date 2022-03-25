fa5dba18b03a9f17ad9222ef6a608e7c79ddf06a
tayleyi <64106592+tayleyi@users.noreply.github.com>
2022-03-24 20:10:03 -0400

    added logic for q/a

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,38 @@
 <script>
-	import * as data from './qa.json';
-	console.log(data);
-	export let question;
-	export let answers;
+	import data from './qa.json';
+	let answers = [];
+	let index = 0;
+	for (let i = 0; i < data[index].answers.length; i++) {
+		answers.push(data[index].answers[i]);
+		answers[i].clicked = false;
+	}
+
+	function handleClick(answerNumber) {
+		answers[answerNumber - 1].clicked = !answers[answerNumber - 1].clicked;
+	}
 </script>
 
 <main>
-	<h1>{ question }</h1>
-	{#each answers as answer}
-		<p>{ answer }</p>
-	{/each}
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Logo_of_Family_Feud.png/250px-Logo_of_Family_Feud.png">
+
+	<h1>{ data[index].question }</h1>
+	<div class="answer-board">
+		{#each answers as answer}
+			<div class="answer" on:click|once={handleClick(answer.number)}>
+				{#if answer.clicked}
+					{ answer.solution }
+				{:else}
+					{ answer.number }
+				{/if}
+			</div>
+		{/each}
+	</div>
 </main>
 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
 	main {
+		font-family: 'Oswald';
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -22,10 +40,22 @@
 	}
 
 	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		font-size: 2em;
+	}
+
+	.answer-board {
+		display: flex;
+		flex-flow: row wrap;
+	}
+
+	.answer {
+		flex: 40%;
+		font-size: 1.5em;
+		background-color: cornflowerblue;
+		color: white;
+		margin: 1%;
+		padding: 1%;
 	}
 
 	@media (min-width: 640px) {
--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		question: 'Name a physician specialty that earns the most money in the medical field.',
-		answers: [
-			"Plastic Surgery",
-			"Orthopedics",
-			"Cardiology",
-			"Uronology",
-			"ENT/Otolaryngology",
-			"Radiology",
-			"Gastroenterology",
-			"Oncology",
-		]
 	}
 });
 
